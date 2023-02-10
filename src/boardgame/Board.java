0bdee702ff87abe0f16fbf73cef93a8c9b95018f
Gabriel Gomes <gabriel.grohl2006@gmail.com>
2023-02-10 08:57:37 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import chess.pieces.Rook;
+
 public class Board {
 	
 	
@@ -37,6 +39,14 @@ public class Board {
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
 	
+	}	
+		
+    public void placePiece(Piece piece, Position position) {
+    	pieces[position.getRow()][position.getColumn()] = piece;
+    	piece.position = position;
+    }	
 
-}
-}
+
+
+		
+	}
