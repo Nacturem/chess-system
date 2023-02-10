f0e29439e54c7cfe46930061d7167355b08e0fd4
Gabriel Gomes <gabriel.grohl2006@gmail.com>
2023-02-10 18:56:15 -0300

    Implementing possible moves of king

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -33,7 +33,7 @@ public class Position {
 	
 	@Override
 	public String toString() {
-		 return row + " , " + column;
+		 return row + ", " + column;
 	
 	
 		 
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -17,15 +17,21 @@ public class ChessMatch {
     
     
     public ChessPiece[][] getPieces() {
-      ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];	   
-      for (int i=0; i<board.getRows(); i++) {	
-    	   for (int j=0; j<board.getColumns(); j++) {	
-    	     mat[i][j] = (ChessPiece) board.piece(i, j);	 
+         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];	   
+             for (int i=0; i<board.getRows(); i++) {	
+    	        for (int j=0; j<board.getColumns(); j++) {	
+    	           mat[i][j] = (ChessPiece) board.piece(i, j);	 
     	
     	   }     
     }
       return mat;
        
+    }
+    
+    public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+    	Position position = sourcePosition.toPosition();
+    	validateSourcePosition(position);
+    	return board.piece(position).possibleMoves();
     }
     
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
