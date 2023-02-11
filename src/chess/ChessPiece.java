a1e8250a7890f62be92313260da3ad7ff11edf7c
Gabriel Gomes <gabriel.grohl2006@gmail.com>
2023-02-11 15:03:27 -0300

    Pawn

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,14 +4,11 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 
-public abstract class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece {
 
-	
-	
 	private Color color;
-    private int moveCount;
-	
-	
+	private int moveCount;
+
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
@@ -21,31 +18,24 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}
 	
-	public void inscreaseMoveCount() {
-		moveCount++;
+	public int getMoveCount() {
+		return moveCount;
 	}
 	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+
 	public void decreaseMoveCount() {
 		moveCount--;
 	}
-	
-	
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
-		
 	}
-
-	
-	
 	
 	protected boolean isThereOpponentPiece(Position position) {
-		ChessPiece p = (ChessPiece)getBoard(). piece(position);
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
-		
 	}
-
-	
-	
-	
-	
 }
