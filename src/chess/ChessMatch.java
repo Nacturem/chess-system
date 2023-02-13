1f1ce600de70896ccb136673de445168061ff89c
Gabriel Gomes <gabriel.grohl2006@gmail.com>
2023-02-13 09:45:47 -0300

    Promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -22,6 +22,8 @@ public class ChessMatch {
 	private boolean check;
 	private boolean checkMate;
 	private ChessPiece enPassantVulnerable;
+	private ChessPiece promoted;
+	
 	
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -59,6 +61,10 @@ public class ChessMatch {
     	return enPassantVulnerable;
     }
     
+    public ChessPiece getPromoted() {
+    	return promoted;
+    }
+    
     
     public ChessPiece[][] getPieces() {
          ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];	   
@@ -92,7 +98,18 @@ public class ChessMatch {
     
     ChessPiece movedPiece = (ChessPiece)board.piece(target);
     
+    //#specialmove promotion
+    promoted = null;
     
+    if(movedPiece instanceof Pawn) {
+    	if ((movedPiece.getColor() ==  Color.WHITE && target.getRow() == 0) || (movedPiece.getColor() ==  Color.BLACK && target.getRow() == 7)) {
+            promoted = (ChessPiece)board.piece(target);
+    		promoted = replacePromotedPiece("Q");
+    		
+    		
+    		
+    }
+    }
     check = (testCheck(opponent(currentPlayer))) ? true : false;
     
     if (testCheck(opponent(currentPlayer))) {
@@ -116,6 +133,24 @@ public class ChessMatch {
     return (ChessPiece)capturedPiece;
     
     }
+    
+    public ChessPiece replacePromotedPiece(String type) {
+    	if (promoted == null) {
+    		throw new IllegalStateException("There is no piece to be promoted");
+    		
+    	}
+    	
+    	
+    	if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+    		throw new InvalidParameterException("Invalid type for promotion");
+    	}
+    	
+    	Position pos = promoted.getChessPosition().toPosition();
+    	Piece p = board.removePiece(pos);
+    	piecesOnTheBoard.remove(p);
+    	
+    }
+    
      
     private Piece makeMove(Position source, Position target) {
     	ChessPiece p = (ChessPiece)board.removePiece(source);
